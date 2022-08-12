3ab6c5ff4c7f351583f6d31ed7b9ea03491be659
Dat Ngo <20078201@tafe.wa.edu.au>
2022-08-12 08:32:15 +0800

    Updated my todo list for the project

---
+++ main.py
@@ -0,0 +1,15 @@
+# Step 1 - Set up local and remote git repositories (Complete)
+
+# Step 2 Create core class and unit tests
+
+# Step 3 Prepare the Double-Linked List implementation
+
+# Step 4 Create the Player class
+
+# Step 5 Implement the Double-Linked List
+
+# Step 6 Add code optimisation
+
+# Step 7 Add Functionality to Double-Linked List
+
+# Step 8 Show entire list
